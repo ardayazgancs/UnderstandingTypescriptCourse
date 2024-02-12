0b2d91006725b3bce150bb291c9c75bdf8c120ad
Arda Yazgan <arda.yazgan@idealo.de>
2024-02-12 15:36:17 +0100

    Adding TypeScript Support with the ts-loader Package

--- DragAndDrop/webpack.config.js
+++ DragAndDrop/webpack.config.js
@@ -4,4 +4,17 @@ module.exports = {
     filename: 'bundle.js',
     path: __dirname + '/dist'
   },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
 };
