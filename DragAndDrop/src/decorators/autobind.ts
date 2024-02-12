5c85a32819ba5700ab703a318d848eb21fe53615
Arda Yazgan <arda.yazgan@idealo.de>
2024-02-12 13:40:38 +0100

    Using ES Modules

--- DragAndDrop/src/decorators/autobind.ts
+++ DragAndDrop/src/decorators/autobind.ts
@@ -1,14 +1,12 @@
-namespace App {
-  // autobind decorator
-  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      enumerable: false,
-      get() {
-        return originalMethod.bind(this);
-      }
-    };
-    return adjDescriptor;
-  }
+// autobind decorator
+export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return originalMethod.bind(this);
+    }
+  };
+  return adjDescriptor;
 }
